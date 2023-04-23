bf1a95119486b4ef4cfaabd3f016d2020708eab0
Edwin Moradian <edwin.moradian90@gmail.com>
2023-04-23 06:11:21 -0700

    working server

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use http::Request;
+use http::Method;
+use server::Server;
+
+mod http;
+mod server;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
